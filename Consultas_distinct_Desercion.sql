ab9b81bcc76c390b9236e7e06441defa8d1b0de8
Jhony-Maldonado <jhonye.maldonados@gmail.com>
2025-05-12 23:30:42 -0500

    Add files via upload

---
+++ Consultas_distinct_Desercion.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT NOM_REGIONAL
+FROM Desercion.dbo.Desercion_Clean_NNN
+order by NOM_REGIONAL 
+ 
+
+SELECT DISTINCT NOM_CENTRO 
+FROM Desercion.dbo.Desercion_Clean_NNN
+order by NOM_CENTRO 
+
+SELECT DISTINCT NOM_PROGRAMA_FORMACION
+FROM Desercion.dbo.Desercion_Clean_NNN
+order by NOM_PROGRAMA_FORMACION 
