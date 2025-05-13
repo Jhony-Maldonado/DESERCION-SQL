44c8811d9ea407f1ce84ec207fc2f0e97a7affda
Jhony-Maldonado <jhonye.maldonados@gmail.com>
2025-05-12 23:38:34 -0500

    Add files via upload

---
+++ consultas_min_max 1.sql
@@ -0,0 +1,6 @@
+--evidencia el minimo y el maximo de muertes en un evento unico
+SELECT MIN(cantidad)
+FROM DataLakeHomicidios.dbo.dl_homicidios
+
+SELECT Max(cantidad)
+FROM DataLakeHomicidios.dbo.dl_homicidios
